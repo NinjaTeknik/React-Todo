ea7d1cbc573209daceb7e3ada9235b8070147294
EOGKevin <104348441+EOGKevin@users.noreply.github.com>
2023-06-20 11:19:34 +0200

    Space

--- server/app.local.js
+++ server/app.local.js
@@ -1,7 +1,6 @@
-const app = require('./auth.app')
-const port = process.env.PORT ?? 8000
+const app = require("./auth.app");
+const port = process.env.PORT ?? 8000;
 
+app.get("/", (req, res) => res.send("Hello!"));
 
-app.get('/', (req, res) => res.send('Hello!'))
-
-app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
